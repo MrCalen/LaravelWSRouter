c28366c6202553a57bda6f179cc168fe1b7b531b
Pierre-Louis Dagues <dagues_p@yaka.epita.fr>
2016-08-04 19:16:19 +0200

    router: fix namespace

--- config/app.php
+++ config/app.php
@@ -156,6 +156,11 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+
+        /*
+         * Calen Service Provider
+         */
+        Calen\Router\RatchetRouterServiceProvider::class,
     ],
 
     /*
--- packages/calen/router/src/RatchetRouterServiceProvider.php
+++ packages/calen/router/src/RatchetRouterServiceProvider.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Providers;
+namespace Calen\Router;
 
 use Illuminate\Support\ServiceProvider;
 
